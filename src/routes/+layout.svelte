ff69b35004414982340d7502888b92ad55474050
M4TThts123 <70703948+M4TThys123@users.noreply.github.com>
2023-07-20 00:52:34 +0200

    🎉  Begin a project.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import './styles.css';
+	import Footer from '$lib/components/Footer.svelte';
+	// import Header2 from '$lib/components/Header2.svelte';
+	import Header03 from '$lib/components/Header03.svelte';
+
+	import type { PageData } from './$types';
+    export let data: PageData;
+</script>
+
+<div class="app">
+	<Header03 stories={data.stories} />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
+
+
